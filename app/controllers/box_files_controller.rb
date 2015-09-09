15c260a660cc3a37c8c3c2c0965a517a7062555c
castaneai <castaneai@castaneai.net>
2015-09-09 14:32:22 +0900

    ファイルアップロード可能に

--- app/controllers/box_files_controller.rb
+++ app/controllers/box_files_controller.rb
@@ -1,7 +1,7 @@
 class BoxFilesController < ApplicationController
   # GET /
   def index
-    @path = Pathname.new(CGI.unescape(params[:path]) || '')
+    @path = Pathname.new(CGI.unescape(params[:path] || ''))
     target = Rails.root.join('data', @path)
     if File.directory?(target)
       @box_files = Dir.foreach(target).reject {|n| n == '.'}.map do |name|
@@ -17,5 +17,20 @@ class BoxFilesController < ApplicationController
     else
       send_file target, disposition: :inline
     end
+  end
+
+  def upload
+    upload_file = params[:upload_file]
+    target_dir = Pathname.new(params[:target_dir])
+    abs_target_dir = Rails.root.join('data', target_dir)
+    return unless upload_file
+    # TODO: file validation
+    # TODO: overwrite check
+    save_path = abs_target_dir.join(upload_file.original_filename)
+    File.open(save_path, "wb") do |f|
+      f.write(upload_file.read)
+    end
+    flash[:notice] = "アップロード成功：#{target_dir.join(upload_file.original_filename)}"
+    redirect_to '/' + target_dir.to_s
   end
 end
